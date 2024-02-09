2331ae52d2f28a3709c939ec914588c4cd806993
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-02-09 21:56:23 +0530

    creating siblings

--- Chapter 01 Inception/Coding/app.js
+++ Chapter 01 Inception/Coding/app.js
@@ -3,11 +3,10 @@
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "I am H2 tag")
-  )
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am H1 tag"),
+    React.createElement("h2", {}, "I am H2 tag"),
+  ])
 );
 
 // const heading = React.createElement(
