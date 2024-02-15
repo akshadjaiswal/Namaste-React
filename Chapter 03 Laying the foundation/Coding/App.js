212a6d18962f0cfb3bebc8ab36435e547adb9908
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-02-15 19:24:38 +0530

    Revising React.createElement

--- Chapter 03 Laying the foundation/Coding/App.js
+++ Chapter 03 Laying the foundation/Coding/App.js
@@ -1,2 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+//React element
+
+const heading = React.createElement(
+  "h1",
+  { id: "root" },
+  "Akshad is learning React js"
+);
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
