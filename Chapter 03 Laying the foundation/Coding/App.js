e8925b0ad6929037d804cbba3358b4fdd9ec7715
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-02-22 14:43:52 +0530

    JSX and CreateElement Mechanism

--- Chapter 03 Laying the foundation/Coding/App.js
+++ Chapter 03 Laying the foundation/Coding/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 //React element
-
+//React.createElement => ReactElement - Js Object=.HTMLelement(render)
 const heading = React.createElement(
   "h1",
   { id: "root" },
@@ -10,6 +10,7 @@ const heading = React.createElement(
 );
 
 //JSX- Where we can merge html and js together // JSX is HTML like sysntax
+//JSX- //React.createElement => ReactElement - Js Object=.HTMLelement(render)
 const jsxheading = <h1 id="heading">Welcome to react learning</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
