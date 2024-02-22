ad4579109df8abb6cda7b5a363743244a9cbaad3
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-02-22 15:13:13 +0530

    JSX differ from HTML
    className
    tabIndex
    Writing in the multiple lines

--- Chapter 03 Laying the foundation/Coding/App.js
+++ Chapter 03 Laying the foundation/Coding/App.js
@@ -12,7 +12,15 @@ const heading = React.createElement(
 //JSX- Where we can merge html and js together // JSX is HTML like sysntax
 //All this is get done with the help of babel of the parcel
 //JSX- //React.createElement => ReactElement - Js Object=.HTMLelement(render)
-const jsxheading = <h1 id="heading">Welcome to react learning</h1>;
+
+//Slight difference in between JSX and HTML
+//For class use (className)
+//for tabindex use (tabIndex)
+const jsxheading = (
+  <h1 id="heading" className="head">
+    Welcome to react learning
+  </h1>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(jsxheading);
