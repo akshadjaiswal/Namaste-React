7f3a4867c4891bdb27b2e2f212a1de8c67b98b47
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-03-02 23:08:55 +0530

    Planning for building an app

---
+++ Chapter 04 Talk is Cheap, show me the code/Coding/App.js
@@ -0,0 +1,15 @@
+//Planning
+/**
+ * 1) Header
+ *  - Log
+ *  - Nav Items
+ * 2)Body
+ *  - Search
+ *  - RestaurantContainer
+ *      - RestaurantCard
+ * 3)Footer
+ *  - Copyrights
+ *  - Link
+ *  - Address
+ *  - Contact
+ */
