642e6e4715a0c6f4c25d2df95a29ee6e1114ffd3
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-03-02 23:38:17 +0530

    Setting Header

--- Chapter 04 Talk is Cheap, show me the code/Coding/App.js
+++ Chapter 04 Talk is Cheap, show me the code/Coding/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 //Planning
 /**
  * 1) Header
@@ -13,3 +15,32 @@
  *  - Address
  *  - Contact
  */
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://icon-library.com/images/order-food-online-icon/order-food-online-icon-13.jpg"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return <div className="app">
+    <Header/>
+  </div>;
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
