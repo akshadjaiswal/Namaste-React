2d8f5fc81465ba51c46d37c38c81f448cdb6926c
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-03-20 13:34:30 +0530

    Named export for multiple exports

--- Chapter 05   Let's get Hooked/Coding/utils/constants.js
+++ Chapter 05   Let's get Hooked/Coding/utils/constants.js
@@ -1,4 +1,5 @@
-const CDN_URL =
+export const CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
-  const LOGO_URL="https://i.pinimg.com/originals/34/0c/6a/340c6add7519212185a08d4205eb1965.png";
+export const LOGO_URL =
+  "https://i.pinimg.com/originals/34/0c/6a/340c6add7519212185a08d4205eb1965.png";
