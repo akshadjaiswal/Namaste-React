4537e1dc2a03873ac9650fb6d73423754087b744
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-03-31 15:29:42 +0530

    filter function

--- Chapter 05   Let's get Hooked/Coding/Components/Body.js
+++ Chapter 05   Let's get Hooked/Coding/Components/Body.js
@@ -1,12 +1,35 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
 const Body = () => {
+  const listOfRestaurants = [
+    {
+      type: "restaurant",
+      data: {
+        id: "74453",
+        name: "Domino's Pizza",
+        cloudinaryImageId: "bz9zkh2aqywjhpankb07",
+        cuisines: ["Pizzas"],
+        costForTwo: 40000,
+        deliveryTime: 45,
+        avgRating: "3.8",
+      },
+    },
+  ];
   return (
     <div className="body">
-      <div className="search"></div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            console.log("button clicked");
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
         {resList.map((restaurant) => (
-          <RestaurantCard key={restaurant.data} resData={restaurant} />
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
     </div>
