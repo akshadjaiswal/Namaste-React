54060e94cb71fd8af0caae4ae53f0354d252088e
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-03-31 16:46:41 +0530

    added top rated restaurant button functionality
    Introduction to react hooks and used useState

--- Chapter 05   Let's get Hooked/Coding/Components/Body.js
+++ Chapter 05   Let's get Hooked/Coding/Components/Body.js
@@ -1,9 +1,11 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
+
 const Body = () => {
-  const listOfRestaurants = [
+  //State variable - Super powerfull variable
+  const [listOfRestaurants, setListOfRestaurants] = useState([
     {
-      type: "restaurant",
       data: {
         id: "74453",
         name: "Domino's Pizza",
@@ -14,21 +16,49 @@ const Body = () => {
         avgRating: "3.8",
       },
     },
-  ];
+    {
+      data: {
+        id: "74454",
+        name: "KFC",
+        cloudinaryImageId: "bz9zkh2aqywjhpankb07",
+        cuisines: ["Pizzas"],
+        costForTwo: 40000,
+        deliveryTime: 45,
+        avgRating: "4.5",
+      },
+    },
+    {
+      data: {
+        id: "74455",
+        name: "MC Donalds",
+        cloudinaryImageId: "bz9zkh2aqywjhpankb07",
+        cuisines: ["Pizzas"],
+        costForTwo: 40000,
+        deliveryTime: 45,
+        avgRating: "4.1",
+      },
+    },
+  ]);
+
   return (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
-            console.log("button clicked");
+            //filter logic
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setListOfRestaurants(filteredList);
+            // console.log(listOfRestaurants);
           }}
         >
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
