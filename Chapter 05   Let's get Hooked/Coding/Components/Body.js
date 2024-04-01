da33afaccbbb37f04fec5241a4332528a6cc3e6d
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-04-01 16:47:28 +0530

    using our mock data

--- Chapter 05   Let's get Hooked/Coding/Components/Body.js
+++ Chapter 05   Let's get Hooked/Coding/Components/Body.js
@@ -4,41 +4,7 @@ import { useState } from "react";
 
 const Body = () => {
   //State variable - Super powerfull variable
-  const [listOfRestaurants, setListOfRestaurants] = useState([
-    {
-      data: {
-        id: "74453",
-        name: "Domino's Pizza",
-        cloudinaryImageId: "bz9zkh2aqywjhpankb07",
-        cuisines: ["Pizzas"],
-        costForTwo: 40000,
-        deliveryTime: 45,
-        avgRating: "3.8",
-      },
-    },
-    {
-      data: {
-        id: "74454",
-        name: "KFC",
-        cloudinaryImageId: "bz9zkh2aqywjhpankb07",
-        cuisines: ["Pizzas"],
-        costForTwo: 40000,
-        deliveryTime: 45,
-        avgRating: "4.5",
-      },
-    },
-    {
-      data: {
-        id: "74455",
-        name: "MC Donalds",
-        cloudinaryImageId: "bz9zkh2aqywjhpankb07",
-        cuisines: ["Pizzas"],
-        costForTwo: 40000,
-        deliveryTime: 45,
-        avgRating: "4.1",
-      },
-    },
-  ]);
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
 
   return (
     <div className="body">
