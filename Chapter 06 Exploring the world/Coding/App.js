3816ef5e10500b432d4153ab9a74e4e4327ccbbf
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-04-10 23:16:42 +0530

    comment

--- Chapter 06 Exploring the world/Coding/App.js
+++ Chapter 06 Exploring the world/Coding/App.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import Header from "./Components/Header";
 import Body from "./Components/Body";
 
-const resObj = {
-  name: "Aaradhana Foods",
-  cuisines: ["Kathiawadi Thali", "Traditional", "Sweet", "Spicy"],
-  avgRating: "4.6",
-  costForTwo: 40000,
-  deliveryTime: 125,
-};
+// const resObj = {
+//   name: "Aaradhana Foods",
+//   cuisines: ["Kathiawadi Thali", "Traditional", "Sweet", "Spicy"],
+//   avgRating: "4.6",
+//   costForTwo: 40000,
+//   deliveryTime: 125,
+// };
 
 const AppLayout = () => {
   return (
