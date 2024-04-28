3f95cd8de29f08393582e6f9801c59d0e078323e
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-04-28 23:12:31 +0530

    Fixed image not loading problem
    Change the restaurant car IMG_CDN_URL

--- Chapter 04 Talk is Cheap, show me the code/Coding/App.js
+++ Chapter 04 Talk is Cheap, show me the code/Coding/App.js
@@ -61,7 +61,7 @@ const RestaurantCard = (props) => {
         className="res-logo"
         alt="res-logo"
         src={
-          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
           cloudinaryImageId
         }
       />
--- Chapter 05   Let's get Hooked/Coding/utils/constants.js
+++ Chapter 05   Let's get Hooked/Coding/utils/constants.js
@@ -1,3 +1,3 @@
-export const CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const LOGO_URL = "https://i.pinimg.com/originals/34/0c/6a/340c6add7519212185a08d4205eb1965.png";
