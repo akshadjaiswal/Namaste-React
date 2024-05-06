84ae2847e9620a8b174db32a41c186862f1d0efc
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-05-07 02:27:39 +0530

    Fetching data from Swiggy api
    Using useEffect

--- Chapter 06 Exploring the world/Coding/Components/Body.js
+++ Chapter 06 Exploring the world/Coding/Components/Body.js
@@ -1,10 +1,22 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
   //State variable - Super powerfull variable
   const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async() => {
+    const data =await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.61610&lng=73.72860&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json= await data.json();
+    console.log(json)
+  };
 
   return (
     <div className="body">
