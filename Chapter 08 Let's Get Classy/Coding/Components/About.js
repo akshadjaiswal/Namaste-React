5f5016ab9e17ab4ad12cd6c8c445218f3ff09566
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-06-21 14:40:47 +0530

    State Variables in Component

--- Chapter 08 Let's Get Classy/Coding/Components/About.js
+++ Chapter 08 Let's Get Classy/Coding/Components/About.js
@@ -1,13 +1,17 @@
 import User from "./User";
 import UserClass from "./UserClass";
-const About = () => {
-  return (
-    <div className="About-div">
-      <h1>About</h1>
-      <User name={"Akshad Jaiswal (function)"} location={"Pune"} />
-      <UserClass name={"Aaradhana (class )"} location={"Pune"} />
-    </div>
-  );
-};
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    return (
+      <div className="About-div">
+        <h1>About Class Component</h1>
+        {/* <User name={"Akshad Jaiswal (function)"} location={"Pune"} /> */}
+        <UserClass name={"Aaradhana (class )"} location={"Pune"} />
+      </div>
+    );
+  }
+}
 
 export default About;
--- Chapter 08 Let's Get Classy/Coding/Components/UserClass.js
+++ Chapter 08 Let's Get Classy/Coding/Components/UserClass.js
@@ -4,12 +4,29 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
     console.log(props);
+    this.state = {
+      count: 0,
+    };
+    console.log("Constructor");
   }
   render() {
     const { name, location } = this.props;
+    const { count } = this.state;
+    console.log("Component Rendered");
     return (
       <div className="user-card">
-        <h1>Member 2</h1>
+        <h1>Count : {count}</h1>
+        <button
+          onClick={() => {
+            //Never Update State Variables directly
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          Update Count
+        </button>
+        <h2>Member </h2>
         <h2>Name: {name}</h2>
         <h3>Position: Marketing Researcher</h3>
         <h3>Location: {location}</h3>
