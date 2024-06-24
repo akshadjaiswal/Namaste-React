eb73418da436fe85b1da4bfe59106a8058ef369b
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-06-24 07:59:23 +0530

    Chapter 09 folder Setup

---
+++ Chapter 09 Optimizing Our App/Coding/Components/Body.js
@@ -0,0 +1,91 @@
+import RestaurantCard from "./RestaurantCard";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+import { Link } from "react-router-dom";
+
+const Body = () => {
+  // console.log("Componenet rerendered");
+  //State variable - Super powerfull variable
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.61610&lng=73.72860&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    // console.log(json);
+
+    setListOfRestaurants(
+      //Optional Chaining
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+
+    setFilteredRestaurant(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  //Conditional Rendering
+  // if (listOfRestaurants.length === 0) {
+  //   return <Shimmer />;
+  // }
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <div className="Search">
+          <input
+            type="text"
+            className="Search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              const filteredRestaurant = listOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            //filter logic
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setFilteredRestaurant(filteredList);
+            // console.log(listOfRestaurants);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {filteredRestaurant.map((restaurant) => (
+          <Link
+            key={restaurant?.info?.id}
+            to={"/restaurants/" + restaurant?.info?.id}
+          >
+            <RestaurantCard resData={restaurant} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Body;
---
+++ Chapter 09 Optimizing Our App/Coding/Components/UserClass.js
@@ -0,0 +1,60 @@
+import React from "react";
+//Class based componenet
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      userInfo: {
+        name: "Dummy",
+        location: "default",
+        bio: "xyz",
+        login: "dummy",
+      },
+    };
+    // console.log(this.props.name + "Child Constructor");
+  }
+  async componentDidMount() {
+    // console.log(this.props.name + "Child Component Did Mount");
+    //API Call
+    const data = await fetch("https://api.github.com/users/akshadjaiswal");
+    const json = await data.json();
+    console.log(json);
+    this.setState({
+      userInfo: json,
+    });
+  }
+  componentDidUpdate() {}
+  componentWillUnmount() {
+    console.log("Component willUnmount");
+  }
+  render() {
+    const { name, bio, location, login, avatar_url } = this.state.userInfo;
+    // const { count } = this.state;
+    // console.log(this.props.name + "Child Component ");
+    return (
+      <div className="user-card">
+        {/* <h1>Count : {count}</h1>
+        <button
+          onClick={() => {
+            //Never Update State Variables directly
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          Update Count
+        </button> */}
+        <h2>Member </h2>
+        <div className="logo-container">
+          <img className="logo" src={avatar_url} />
+        </div>
+        <h2>Name: {name}</h2>
+        <h3>Bio: {bio}</h3>
+        <h3>Location: {location}</h3>
+        <h4>Github: {login}</h4>
+      </div>
+    );
+  }
+}
+export default UserClass;
