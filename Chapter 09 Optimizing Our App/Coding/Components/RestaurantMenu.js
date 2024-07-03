4c8e0ea7459cfb3c92d1775a0bc6ceebc2a45c93
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-07-03 20:56:53 +0530

    Creating custom hooks

--- Chapter 09 Optimizing Our App/Coding/Components/RestaurantMenu.js
+++ Chapter 09 Optimizing Our App/Coding/Components/RestaurantMenu.js
@@ -1,22 +1,10 @@
-import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import { MENU_API_URL } from "../utils/constants";
+import useRestaurantmenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
-
   const { resId } = useParams();
-
-  useEffect(() => {
-    fetchMenu();
-  }, []);
-
-  const fetchMenu = async () => {
-    const data = await fetch(MENU_API_URL + resId);
-    const json = await data.json();
-    setResInfo(json.data);
-  };
+  const resInfo = useRestaurantmenu(resId);
 
   if (resInfo === null) return <Shimmer />;
 
