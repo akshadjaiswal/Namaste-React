b5ae6df83191d4b375d068e820e04fd0cdd137dd
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-07-31 20:37:59 +0530

    Building Accordion

--- Chapter 11  Data Is The New Oil/Coding/Components/ItemList.js
+++ Chapter 11  Data Is The New Oil/Coding/Components/ItemList.js
@@ -1,3 +1,5 @@
+import { CDN_URL } from "../utils/constants";
+
 const ItemList = ({ items }) => {
   console.log(items);
   return (
@@ -6,13 +8,26 @@ const ItemList = ({ items }) => {
         {items.map((item) => (
           <div
             key={item.card.info.id}
-            className="p-2 m-2 border-gray-200 border-b-2"
+            className="p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between"
           >
-            <div>
-              <span className="font-semibold">{item.card.info.name}</span>
-              <span> - ₹{item.card.info.price / 100}</span>
+            <div className="w-9/12">
+              <div classN ame="py-3">
+                <span className="text-lg font-bold">{item.card.info.name}</span>
+                <span> - ₹{item.card.info.price / 100}</span>
+              </div>
+              <p className="text-[15px]">{item.card.info.description}</p>
             </div>
-            <p className="text-sm">{item.card.info.description}</p>
+            <div className="w-4/12 p-2 ">
+              <div className=" absolute">
+                <button className="p-2 shadow-lg bg-white mx-16 rounded-lg">
+                  Add +
+                </button>
+              </div>
+              <img
+                className="rounded-md"
+                src={CDN_URL + item.card.info.imageId}
+              ></img>
+            </div>
           </div>
         ))}
       </ul>
