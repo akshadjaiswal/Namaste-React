fadf28c3def9107a0d16846e8f997774ae7437e4
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-12-31 23:44:22 +0530

    doc: File name changed :memo:

--- Chapter 12  Let's Build Our Store/Coding/utils/cartSlice.js
+++ Chapter 12  Let's Build Our Store/Coding/utils/cartSlice.js
@@ -9,10 +9,10 @@ const cartSlice = createSlice({
         addItem: (state, action) => {
             state.items.push(action.payload)
         },
-        removeItem: (state, action) => {
+        removeItem: (state) => {
             state.items.pop();
         },
-        clearCart: (state, action) => {
+        clearCart: (state) => {
             state.items.length = 0;
         }
     }
